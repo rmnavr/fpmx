d7d64132098041ec3d8a9c53c5758823f0f71b69
Pupos <averrmn@gmail.com>
2026-03-07 20:18:55 +0300

    starting rebranding into FPTK->FPMX

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools                 import setup, find_packages
 from setuptools.command.install import install 
 
-proj_version = '0.5.0.dev5'
+proj_version = '0.5.1.dev1'
 
 libs_required = [
     'hy >= 1',
